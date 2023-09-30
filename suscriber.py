1668ad4e665ee566fb8af7245ec5a7c752409ee0
Eduardo Herrera <78046940+eduardoh27@users.noreply.github.com>
2023-09-29 20:29:12 -0500

    safd

--- suscriber.py
+++ suscriber.py
@@ -6,6 +6,10 @@ import django
 from devices.logic.logic_device import create_device_object
 from sites.services.services_sites import get_site
 
+site_list = ['Marly', 'Country', 'Santa Fé', 'San Ignacio', 'Policlínica', 'Reina Sofía']
+builder_list = ['LG', 'Metronik', 'Samsung', 'Challenger', 'SuperSanaloTodo']
+name_list = ['TAC', 'Rayos X', 'MRI', 'Ventilador', 'Respirador', 'Electroalgo']
+
 path.append('monitoring/settings.py')
 environ.setdefault('DJANGO_SETTINGS_MODULE', 'monitoring.settings')
 django.setup()
@@ -13,15 +17,14 @@ django.setup()
 
 print('Iniciando la carga de los 10 devices ...')
 
-def callback(method, body):
-    payload = json.loads(body.decode('utf8').replace("'", '"'))
-    topic = method.routing_key.split('.')
-    variable = get_site(topic[2])
-    create_device_object(
-        variable, payload['value'], payload['unit'], topic[0] + topic[1])
-
 for i in range(10):    
-    site = get_site("marly")
-    value = random.randint(0, 20)
+    site = random.choice(site_list)
+    active = random.choice([True, False])
+    code = random.randint(100, 1000)
+    builder = random.choice(builder_list)
+    name = random.choice(name_list)
+    amount = random.randint(0, 21)
+    type = random.choice(name_list)
+
     create_device_object(
-        site, value, 'C', 'ML.505')
+        site, active, code, builder, name, amount, type)

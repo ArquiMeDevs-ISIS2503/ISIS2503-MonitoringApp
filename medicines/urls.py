906d718dd88ca8395edb9ed68699b1d7695e0eee
dcuevasa <d.cuevas@uniandes.edu.co>
2023-10-07 11:15:42 -0500

    urlFix

--- medicines/urls.py
+++ medicines/urls.py
@@ -4,6 +4,6 @@ from django.views.decorators.csrf import csrf_exempt
 from . import views
 
 urlpatterns = [
-    path('medicines/', views.site_list, name='medicineList'),
-    path('medicinecreate/', csrf_exempt(views.site_create), name='medicineCreate'),
+    path('medicines/', views.medicine_list, name='medicineList'),
+    path('medicinecreate/', csrf_exempt(views.medicine_create), name='medicineCreate'),
 ]

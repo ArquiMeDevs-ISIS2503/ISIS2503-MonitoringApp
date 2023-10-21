cb3151b192ea6e42e52a8061b15e80562314713f
dcuevasa <d.cuevas@uniandes.edu.co>
2023-10-21 10:47:08 -0500

    .data

--- medicines/views.py
+++ medicines/views.py
@@ -17,7 +17,7 @@ def medicine_create(request):
         form = MedicineForm(request.POST)
         print(request)
         print(form)
-        print(request.text)
+        print(request.data)
         if form.is_valid():
             create_medicine(form)
             messages.add_message(request, messages.SUCCESS, 'Successfully created medicine')

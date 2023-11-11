93f16c8b10971c061ff4c95095aef9630d7f95b9
dcuevasa <d.cuevas@uniandes.edu.co>
2023-11-11 11:12:29 -0500

    dict access

---
+++ entrys/templatetags/pages_extras.py
@@ -0,0 +1,11 @@
+from django import template
+from entrys.models import Entry
+
+register = template.Library()
+
+@register.filter
+def keyvalue(dict, key):    
+    try:
+        return dict[key]
+    except KeyError:
+        return ''

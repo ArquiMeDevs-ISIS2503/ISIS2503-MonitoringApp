43c7e35a1295066708f7647ad0ead06dc32c6d15
Eduardo Herrera <78046940+eduardoh27@users.noreply.github.com>
2023-12-06 16:29:50 -0500

    .

--- devices/views.py
+++ devices/views.py
@@ -69,10 +69,11 @@ def device_create1(request):
 def device_create(request):
     if request.method == 'POST':
         form = DeviceForm(request.POST)
+        print(form.cleaned_data)
         if form.is_valid():
             # Suponiendo que el microservicio espera los mismos datos que DeviceForm
+            print('form is valid')
             response = requests.post('http://104.197.122.243:8080/create_device/', data=form.cleaned_data)
-            print(form.cleaned_data)
 
             if response.status_code == 201:
                 messages.add_message(request, messages.SUCCESS, 'Device create successful')
